612bfe985d4d91df63cbdd80b1b501f379eb0b7d
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-07 12:23:07 -0500

    app terminada.

--- src/App.js
+++ src/App.js
@@ -21,13 +21,12 @@ function App() {
     <div className="App">
       <header className="App-header">
 
+        <h1 className='title'>Rick & Morty</h1>
+
         {characters ? (
-          <Characters characters={characters} />
+          <Characters characters={characters} setCharacters={setCharacters} />
         ) : (
           <>
-            <h1 className='title'>Rick & Morty</h1>
-
-
             <img src={imageRickMorty} alt="Rick & Morty" className='img-home'></img>
 
             <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
